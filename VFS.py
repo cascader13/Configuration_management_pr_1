3dbf062575b776a9debc2bb1622a47a7bf311f3f
cascader13 <AlekseiKhitrov@yandex.ru>
2025-09-13 13:14:30 +0300

    Организовал структуру класса

---
+++ VFS.py
@@ -0,0 +1,29 @@
+import os
+import re
+import shlex
+import argparse
+
+
+class VFS:
+    def __init__(self):
+        pass
+
+    def process(self):
+        pass
+
+    def ls(self, path: str):
+        pass
+
+    def cd(self, path: str):
+        pass
+
+    def exit(self):
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
