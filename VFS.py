98c2f0bfbe8357d6c296ff291148d168b21ae634
cascader13 <AlekseiKhitrov@yandex.ru>
2025-09-13 13:47:17 +0300

    Расширена структура класса. Прописана реализация метода для обработки команд

--- VFS.py
+++ VFS.py
@@ -6,23 +6,45 @@ import argparse
 
 class VFS:
     def __init__(self):
-        pass
-
+        self.currentpath = ""
+        self.name = "VFS"
     def process(self):
-        pass
+        while True:
+            cmd = input(f'{self.name}:{self.currentpath}\$').split(" ")
+            if cmd[0] == "exit":
+                self.exit()
+            elif cmd[0] == "ls":
+                try:
+                    if not self.ls(cmd[1]):
+                        print(f'Directory "{cmd[1]}" if not exist.')
+                except:
+                    self.cd("")
+            elif cmd[0] == "cd":
+                try:
+                    if not self.cd(cmd[1]):
+                        print(f'Directory "{cmd[1]}" not exist.')
+                except:
+                    self.cd("")
+            elif cmd[0][0] == '$':
+                self.local_variables(cmd[0][1::])
+            else:
+                print("unknown command")
 
+    def local_variables(self, name: str):
+        print("I local variable", name)
     def ls(self, path: str):
-        pass
+        print("i ls")
 
     def cd(self, path: str):
-        pass
+        print("i cd")
 
     def exit(self):
-        pass
+        exit(0)
 
 
 def main():
-    pass
+    vfs = VFS()
+    vfs.process()
 
 
 if __name__ == '__main__':
